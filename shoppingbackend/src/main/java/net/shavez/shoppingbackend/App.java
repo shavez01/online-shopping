490ac6fbc06f5cc6888607bc27a38814a1930cc6
Shavez Mohammad <shvz.mohammad@gmail.com>
2017-12-24 13:29:51 +0530

    My Project Setup Completed Successfully

---
+++ shoppingbackend/src/main/java/net/shavez/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.shavez.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
